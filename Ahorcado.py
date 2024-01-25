63b7044e96c0b255064321917ba985c5559bb67d
Borja Rodriguez <borjarodriguez203@gmail.com>
2024-01-25 21:11:24 +0100

    Segundo commit, se agregan 2 funciones, la que muestra el tablero y la que controlara si una letra es adivinada o no

--- Ahorcado.py
+++ Ahorcado.py
@@ -19,3 +19,20 @@ class Ahorcado:
 
 
         ]
+
+    # Verificamos en este if si el número de intentos actual (self.intentos)
+    # que la longitud de la lista figura, y mostramos la representación gráfica correspondiente a
+    # la posición actual de la etapa del juego
+        if self.intentos < len(figura):
+            print(figura[self.intentos])
+
+    # Este método que creo es para procesar cada letra dada por el jugador está dentro de
+    # la palabra que el programa a elegido de forma aleatoria dentro de las palabras dadas en el
+    # atributo (self.palabras) si esto no se cumple se reduce el numero de intentos en -1
+
+    def adivinar_letra(self, letra):
+        if letra in self.palabra and letra not in self.letras_adivinadas:
+            self.letras_adivinadas.append(letra)
+        else:
+            self.intentos -= 1
+
